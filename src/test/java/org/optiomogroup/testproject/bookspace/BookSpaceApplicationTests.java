ab784458dd37634bb7a33c961543a371c3822368
Lado <maisuradze.vm@gmail.com>
2024-03-08 19:25:32 +0400

    first commit

---
+++ src/test/java/org/optiomogroup/testproject/bookspace/BookSpaceApplicationTests.java
@@ -0,0 +1,13 @@
+package org.optiomogroup.testproject.bookspace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookSpaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
